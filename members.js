7eeaad73a6ffbc56d7d09f3fe8c3a5c00d1581d1
Ariel Falcón <121772005+ArielFalcon@users.noreply.github.com>
2024-12-18 18:51:47 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML',
+            level: 9
+        },
+        {
+            name: 'CSS',
+            level: 7
+        },
+        {
+            name: 'JS',
+            level: 8
+        }
+    ];
+    return skills;
+}
